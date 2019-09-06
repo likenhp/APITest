361305b336980d0bfc37ba82d7d902d5f7fab56e
Kenneth Li <likenhp@gmail.com>
2019-09-06 14:18:15 -0700

    need to test the create and delete functions

--- api.js
+++ api.js
@@ -6,11 +6,15 @@ class APIData{
         this.number = null;
 
         //this.value = this.value.bind(this);
-        this.findAnimal = this.findAnimal.bind(this);
+
         this.handleAPIData = this.handleAPIData.bind(this);
         this.handleAPIDataSuccess = this.handleAPIDataSuccess.bind(this);
         this.handleAnimalSearch = this.handleAnimalSearch.bind(this);
         this.handleAnimalSearchSuccess = this.handleAnimalSearchSuccess.bind(this);
+        this.handlecreateAnimal = this.handlecreateAnimal.bind(this);
+        this.handlecreateAnimalSuccess = this.handlecreateAnimalSuccess.bind(this);
+        this.handleAnimalDelete = this.handleAnimalDelete.bind(this);
+        this.handleAnimalDeleteSuccess = this.handleAnimalDeleteSuccess.bind(this);
         this.handleAPIError = this.handleAPIError.bind(this);
         this.displayData = this.displayData.bind(this);
     }
@@ -18,11 +22,19 @@ class APIData{
     addEventHandlers(){
         $("#getDataButton").on("click", this.handleAPIData);
         $("#findAnimalButton").on("click", this.findAnimal);
+        $("#createButton").on("click", this.createAnimal);
+        $("#deleteButton").on("click", this.deleteAnimal);
     }
 
-    findAnimal() {
+    findAnimal = () => {
         $("#animalContent").empty();
-        this.value = $("#findAnimal").val();
+        this.value = $("#findAnimal").val().trim();
+        /* Test to ensure no field is empty */
+        if(this.value === ""){
+            alert("Must fill out field");
+            return;
+        }
+        /* Test to ensure the id is a number */
         this.mustBeNumber = /^[0-9]+$/;
         this.regex = new RegExp(this.mustBeNumber);
         if( this.regex.test(Number(this.value)) && Number(this.value)>0){
@@ -31,6 +43,58 @@ class APIData{
             alert("Must use a valid number for id search. Number must be greater than 0 with no decimals or special characters");
         }
         
+    }
+
+    createAnimal = () => {
+        $("#createContent").empty();
+        this.commonName = $("#commonName").val();
+        this.scientificName = $("#scientificName").val();
+        this.family = $("#family").val();
+        this.imageURL = $("#imageURL").val();
+        /* Test to ensure no field is empty */
+        if(this.commonName === "" || this.scientificName === "" || this.family === "" || this.imageURL === ""){
+            alert("Must fill out all fields");
+            return;
+        }
+        debugger;
+        /* Checking to see if the values are strings and not numbers*/
+        this.mustBeNumber = /^[0-9]+$/;
+        this.regex = new RegExp(this.mustBeNumber);
+        if(typeof this.commonName !== "string" || this.regex.test(this.commonName)){
+            alert("Common Name not valid");
+            return;
+        }
+        if(typeof this.scientificName !== "string" || this.regex.test(this.scientificName)){
+            alert("Scientific Name not valid");
+            return;
+        }
+        if(typeof this.family !== "string" || this.regex.test(this.family)){
+            alert("Family Name not valid");
+            return;
+        }
+        // if(/^https?:\/\//.test(this.imageURL)){
+        //     alert("imageURL not valid");
+        //     return;
+        // }
+
+        this.handlecreateAnimal(this.commonName, this.scientificName, this.family, this.imageURL);
+    }
+
+    deleteAnimal = () =>{
+        this.animalId = $("#deleteData").val();
+
+        if(this.animalId === ""){
+            alert("Must fill out field");
+            return;
+        }
+        /* Test to ensure the id is a number */
+        this.mustBeNumber = /^[0-9]+$/;
+        this.regex = new RegExp(this.mustBeNumber);
+        if( this.regex.test(Number(this.animalId)) && Number(this.animalId)>0){
+            this.handleAnimalDelete((Number(this.animalId)));
+        }else{
+            alert("Must use a valid number for id search. Number must be greater than 0 with no decimals or special characters");
+        }
     }
 
     displayData(){
@@ -92,7 +156,6 @@ class APIData{
             this.family = $("<div>",{"class": "scienceName"}).text(resp.animal.family)
             this.animal.append(this.animalId, this.animalCommonName, this.scienceName, this.family, this.imageURL);
             $("#animalContent").append(this.animal);
-
             if($("#animalContent").is(":visible") !== true){
                 $("#animalContent").show();
             }
@@ -100,6 +163,45 @@ class APIData{
             $("#animalContent").text(resp.status);
             $("#animalContent").show();
         }
+    }
+
+    handlecreateAnimal(commonName, scientificName, family, imagerURL){
+        $.ajax({
+            url: "https://animalrestapi.azurewebsites.net/Animal/Create",
+            method: "post",
+            data: {
+                "candidateID": "13b1bdc9-586d-436e-958d-f64827035d8d",
+                "commonName": commonName,
+                "scientificName": scientificName,
+                "family": family,
+                "imageURL": imagerURL
+            },
+            dataType: "json",
+            success: this.handlecreateAnimalSuccess,
+            error: this.handleAPIError
+        });
+    }
+
+    handlecreateAnimalSuccess(resp){
+        console.log(resp);
+    }
+
+    handleAnimalDelete(id){
+        $.ajax({
+            url: "https://animalrestapi.azurewebsites.net/Animal/Delete",
+            method: "post",
+            data: {
+                "candidateID": "13b1bdc9-586d-436e-958d-f64827035d8d",
+                "id": id
+            },
+            dataType: "json",
+            success: this.handleAnimalDeleteSuccess,
+            error: this.handleAPIError
+        });
+    }
+
+    handleAnimalDeleteSuccess(resp){
+        console.log(resp);
     }
 
     handleAPIError(resp){
